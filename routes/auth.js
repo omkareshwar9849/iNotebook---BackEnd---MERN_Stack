6e02fa95f4d953df2d7b9ba3f24b122d6f5f5022
omkareshwar9849 <omkar.varada9849@gmail.com>
2023-04-29 16:19:05 +0530

    v50 completed

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,8 @@ const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+//createing secret for jwt authentication
+const JWT_SECRET = "This project is done by $omkar";
 
 // Create a User using: POST "/api/auth/createuser". No login required
 router.post('/createuser', [
@@ -33,8 +35,7 @@ router.post('/createuser', [
       password: secPass,
       email: req.body.email,
     })
-    //createing secret for jwt authentication
-    const JWT_SECRET = "This project is done by $omkar";
+
     //creating a jwt token fot authentication
     const data = {
       user :{
@@ -42,13 +43,53 @@ router.post('/createuser', [
       }
     }
     const authtoken = jwt.sign(data,JWT_SECRET);
-    res.json({authtoken})
 
     // res.json(user)
+    res.json({authtoken})
     
   } catch (error) {
     console.error(error.message);
-    res.status(500).send("Some Error occured");
+    res.status(500).send("Internal server Error occured");
+  }
+})
+
+
+// Authenticating a User using: POST "/api/auth/login". No login required
+router.post('/login', [
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+  // If there are errors, return Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {email,password} = req.body;
+  try {
+
+    // Check whether the user with this email exist or not
+    let user = await User.findOne({ email});
+    if (!user) {
+      return res.status(400).json({ error: "Please try to login with correct crendentials" });
+    }
+    //verifying the password using bcrypt
+    const passwordCompare = await bcrypt.compare(password,user.password);
+    if(!passwordCompare){
+      return res.status(400).json({ error: "Please try to login with correct crendentials" });
+    }
+
+    const data = {
+      user :{
+        id : user.id
+      }
+    }
+    const authtoken = await jwt.sign(data,JWT_SECRET);
+    res.json({authtoken})
+    
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server Error occured");
   }
 })
 
